92de41890f1951c1caedd4e149600ae4de93a9ac
Juan <juand.arpi@ucuenca.edu.ec>
2026-04-24 22:21:14 -0500

    Ingreso de archivos base

---
+++ Gestion de practicas preprofesionlaes/Modelo/Usuario.py
@@ -0,0 +1,6 @@
+class Usuario():
+    def __init__(self,id_usuario,correo,clave,rol):
+        self.id_usuario = id_usuario
+        self.correo= correo
+        self.clave = clave
+        self.rol = rol
